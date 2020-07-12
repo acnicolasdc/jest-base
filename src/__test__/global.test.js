50bccadd63c6f71e21e0bb08cd24171174e90329
acnicolasdc <nicolasreyesbejarano@gmail.com>
2020-07-12 17:57:53 -0500

    Testing Promises with Jest

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -6,6 +6,19 @@ const reverseString = (str, callback) => {
     callback(str.split("").reverse().join(""))
 }
 
+const reverseStringPromise = (str) => {
+ return new Promise((resolve, reject) => {
+     if(!str) reject(Error('Variable not Found'));
+     resolve(str.split("").reverse().join(""));
+ })
+}
+
+test('Testing Promise', () => {
+    reverseStringPromise('Hello').then( string => {
+        expect(string).toBe('olleH');
+    })
+});
+
 test('Testing Callback', () => {
     reverseString('Hello', (str) => {
         expect(str).toBe('olleH');
