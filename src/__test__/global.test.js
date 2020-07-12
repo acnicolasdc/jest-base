ac6e711a3f3d5eba04deff7c817b473a09a405d7
acnicolasdc <nicolasreyesbejarano@gmail.com>
2020-07-12 17:51:35 -0500

    Testing Callbacks with Jest

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -2,6 +2,15 @@ const string = 'Hi everybody';
 const array = ['apple', 'pineapple', 'banana'];
 const number = 10;
 const boolean = true;
+const reverseString = (str, callback) => {
+    callback(str.split("").reverse().join(""))
+}
+
+test('Testing Callback', () => {
+    reverseString('Hello', (str) => {
+        expect(str).toBe('olleH');
+    })
+});
 
 test('Its must be a String', () => {
     expect(string).toMatch(/everybody/);
