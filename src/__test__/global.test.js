356726e352e88e5341e8fe163f3bbeb17a06ff55
acnicolasdc <nicolasreyesbejarano@gmail.com>
2020-07-12 17:40:47 -0500

    Testing arrays with jest

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,10 @@
 const text = 'Hi everybody';
+const fruits = ['apple', 'pineapple', 'banana'];
 
 test('its must be a String', () => {
     expect(text).toMatch(/everybody/);
 });
+
+test('its must contain a banana string', ()=>{
+    expect(fruits).toContain('banana');
+})
