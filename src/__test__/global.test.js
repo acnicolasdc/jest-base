6167a123c750c2ce0cead24600ec3d84d04eb9f6
acnicolasdc <nicolasreyesbejarano@gmail.com>
2020-07-12 17:43:39 -0500

    Testing numbers with Jest

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,6 @@
 const text = 'Hi everybody';
 const fruits = ['apple', 'pineapple', 'banana'];
+const number = 10;
 
 test('its must be a String', () => {
     expect(text).toMatch(/everybody/);
@@ -8,3 +9,7 @@ test('its must be a String', () => {
 test('its must contain a banana string', ()=>{
     expect(fruits).toContain('banana');
 })
+
+test('Greater than', ()=>{
+    expect(number).toBeGreaterThan(9);
+});
