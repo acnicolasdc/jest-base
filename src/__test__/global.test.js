c23972499bc4f43dc0e719fac941ee596d3b5605
acnicolasdc <nicolasreyesbejarano@gmail.com>
2020-07-12 17:46:10 -0500

    Testing booleans with Jest

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,15 +1,20 @@
-const text = 'Hi everybody';
-const fruits = ['apple', 'pineapple', 'banana'];
+const string = 'Hi everybody';
+const array = ['apple', 'pineapple', 'banana'];
 const number = 10;
+const boolean = true;
 
-test('its must be a String', () => {
-    expect(text).toMatch(/everybody/);
+test('Its must be a String', () => {
+    expect(string).toMatch(/everybody/);
 });
 
-test('its must contain a banana string', ()=>{
-    expect(fruits).toContain('banana');
+test('Its must contain a banana string', ()=>{
+    expect(array).toContain('banana');
 })
 
 test('Greater than', ()=>{
     expect(number).toBeGreaterThan(9);
+});
+
+test('Its must be true', ()=>{
+    expect(boolean).toBeTruthy();
 });
